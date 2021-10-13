d4aff1051d9f13001a572bcbfae40fa9ae7f03f5
HoneyGump <honeygump@qq.com>
2021-10-13 16:14:47 +0800

    add DC

--- Photonics_PDK_v1.py
+++ Photonics_PDK_v1.py
@@ -516,13 +516,54 @@ def gene_ring(lib, cellName='Ring', radius_ring=10, gap=0.1, l_wg=40, w_wg=0.45)
     convert_to_positive_resist(ring, [poly, arc], buffer_radius=3)
 
     return ring
-            
+
+# define direction coupler
+def DC(lib, cellName='DC', l_DC=20, spacing=0.6, radius_bend=10, l_ver=10, w_wg=0.5):
+    l_PortIn = 10
+    cell = lib.new_cell(cellName)
+    
+    path_dc = gdspy.Path(w_wg, (0,0))
+    path_dc.segment(l_PortIn)
+    path_dc.turn(radius_bend,'r')
+    path_dc.segment(l_ver)
+    path_dc.turn(radius_bend,'l')
+    path_dc.segment(l_DC)
+    path_dc.turn(radius_bend,'l')
+    path_dc.segment(l_ver)
+    path_dc.turn(radius_bend,'r')
+    path_dc.segment(l_PortIn)
+    cell.add(path_dc)
+
+    path_dc = gdspy.Path(w_wg, (0, -(l_ver*2+radius_bend*4+spacing)))
+    path_dc.segment(l_PortIn)
+    path_dc.turn(radius_bend,'l')
+    path_dc.segment(l_ver)
+    path_dc.turn(radius_bend,'r')
+    path_dc.segment(l_DC)
+    path_dc.turn(radius_bend,'r')
+    path_dc.segment(l_ver)
+    path_dc.turn(radius_bend,'l')
+    path_dc.segment(l_PortIn)
+    cell.add(path_dc)
+
+    return cell
+
+# define direction coupler2
+def DC2(lib, cellName='DC', l_DC=20, spacing=0.6, radius_bend=10, l_ver=10, w_wg=0.5, w_etch=3 ):
+    cell1 = DC(lib, 'temp1_'+cellName, l_DC, spacing, radius_bend, l_ver, w_wg)
+    cell2 = DC(lib, 'temp2_'+cellName, l_DC, spacing, radius_bend, l_ver, w_wg+2*w_etch)
+    lib.remove(cell1)
+    lib.remove(cell2)
+    cell = lib.new_cell(cellName)
+    poly = gdspy.boolean(cell1, cell2, 'xor', **ld_fulletch)
+    cell.add(poly)
+    return cell
 
 if __name__ == '__main__':
     lib = gdspy.GdsLibrary( precision=1e-10)
-    gene_ring(lib, 'Ring')
+    DC2(lib, 'Ring')
     gdspy.LayoutViewer()
     # test = Ring_4port(1,1)
     # cell.add(test.place_ring())
     # print(test.port)
-    # lib.write_gds('test_ring.gds')
+    lib.write_gds('test_ring.gds')
